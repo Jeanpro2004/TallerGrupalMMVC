0dc87b809e5adabf00cbc1a9ee0ee4693193aa0e
laboratorio <laboratorio@UPOAULA10714>
2025-06-18 08:47:50 -0500

    Agregar archivos de proyecto.

---
+++ TallerGrupalMMVC/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TallerGrupalMMVC;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
